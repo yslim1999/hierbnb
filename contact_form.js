e07301b682fe99e9b7561d64bf5029c8f6e5f04c
Yin Lim <yin.lim-2@student.manchester.ac.uk>
2022-01-04 12:29:25 +0000

    sign up and contact brush up

--- contact_form.js
+++ contact_form.js
@@ -1,10 +1,14 @@
 function contact_check(){
-    var fname = document.getElementById("fname");
-    var lname = document.getElementById("lname");
-    var country = document.getElementById("country");
-    var subject = document.getElementById("subject");
 
-    if (!fname.checkValidity()) {
-        document.getElementById("error").innerHTML = "Hello Everyone";
-    }
+        var fname = document.getElementById("fname");
+        var lname = document.getElementById("lname");
+        var country = document.getElementById("country");
+        var subject = document.getElementById("subject");
+
+        if(typeof fname !== "undefined" && fname.value == '' && typeof lname !== "undefined" && lname.value == '' && typeof country !== "undefined" && country.value == '' && typeof subject !== "undefined" && subject.value == ''){
+            return;
+        }
+        document.getElementById("whole_container").style.visibility = "hidden";
+        document.getElementById("status_thing").innerHTML = "Thank you " + fname.valueOf() +", We have received your feedback. We will get in touch with you soon."
+        document.getElementById("feedbackbox").style.display = "block";
 }
