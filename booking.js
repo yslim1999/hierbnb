a299aae1da29a62fb7128d9ea2232550f8636cfb
jorge <jorgeandresilva1997bc@gmail.com>
2022-01-05 10:46:33 +0000

    bookings working(attempt to validate dates)+navbar

--- booking.js
+++ booking.js
@@ -15,29 +15,35 @@ function storeBooking(){ //stores bookings in the localStorage
 
     console.log(checkIn);
     console.log(checkOut);
-    if(window.localStorage.getItem("bookings")){
-        var bookings = window.localStorage.getItem("bookings");
+    if(window.localStorage.getItem(window.localStorage.getItem("log_status")+"bookings")){
+        var bookings = window.localStorage.getItem(window.localStorage.getItem("log_status")+"bookings");
         bookings = bookings.concat("<tr><th>"+location+"</th><td>"+guests+"</td><td>"+checkIn+"</td><td>"+checkOut+"</td><td>Team member is reviewing your booking and will call you as soon as possible</td></tr>");
-        window.localStorage.setItem("bookings",bookings);
+        window.localStorage.setItem((window.localStorage.getItem("log_status")+"bookings"),bookings);
         console.log("doing this");
     }
     else{
         var tableHeaders = '<table class="table table-striped" style="color: white;"><thead style="background-color: rgb(41, 40, 40);"><tr><th scope="col">Location</th><th scope="col">Number of guests</th><th scope="col">Check-in Date</th><th scope="col">Check-out date</th><th scope="col">Comments</th></tr></thead><tbody style="background-color: white;color: black;">';
         var booking = "<tr><th>"+location+"</th><td>"+guests+"</td><td>"+checkIn+"</td><td>"+checkOut+"</td><td>Team member is reviewing your booking and will call you as soon as possible</td></tr>";
-        window.localStorage.setItem("bookings",tableHeaders.concat(booking));
+        window.localStorage.setItem((window.localStorage.getItem("log_status")+"bookings"),tableHeaders.concat(booking));
         console.log("doing that")
     }
 
-    console.log(window.localStorage.getItem("bookings"));
-    window.localStorage.setItem("user","jorge");
-    console.log(window.localStorage.getItem("user"));
 }
 
 function loadBookings(){
 
     console.log("working")
-    if(window.localStorage.getItem("bookings")){
-        console.log(window.localStorage.bookings.concat("</tbody></table>"));
-        document.getElementById("displayBookings").innerHTML= window.localStorage.bookings.concat("</tbody></table>");
+    if(window.localStorage.getItem((window.localStorage.getItem("log_status")+"bookings"))){
+        var bookings = window.localStorage.getItem((window.localStorage.getItem("log_status")+"bookings"));
+        console.log(bookings.concat("</tbody></table>"));
+        document.getElementById("displayBookings").innerHTML= bookings.concat("</tbody></table>");
     }
+
+    if(!window.localStorage.getItem("log_status")){
+        document.getElementById("makeBooking").innerHTML='<button id="makeBooking" class="submit_button" data-toggle="tooltip" data-placement="top" title="Please log in to place a booking"><span>Submit</span></button>'
+    }
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
 }
